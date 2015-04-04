03b0b65845f99c3cbd8ccfe9c16d2e4822e084e3
Siarhei Hanchuk <siarheihanchuk@gmail.com>
2015-04-04 23:14:14 +0300

    Edit product stores

--- app/controllers/spree/admin/products_controller_decorator.rb
+++ app/controllers/spree/admin/products_controller_decorator.rb
@@ -1,6 +1,9 @@
 Spree::Admin::ProductsController.class_eval do
   update.before :set_stores
 
+  def stores
+  end
+
   private
   def set_stores
     @product.store_ids = nil unless params[:product].key? :store_ids
--- app/overrides/decorate_admin_products_form.rb
+++ app/overrides/decorate_admin_products_form.rb
@@ -1,6 +1,8 @@
 Deface::Override.new(
-  :virtual_path => "spree/admin/products/_form",
+  :virtual_path => "spree/admin/shared/_product_tabs",
   :name => "multi_domain_admin_product_form_meta",
-  :insert_bottom => "[data-hook='admin_product_form_meta']",
-  :partial => "spree/admin/products/stores",
+  :insert_bottom => "[data-hook='admin_product_tabs']",
+  text: "<%= content_tag :li, :class => ('active' if current == 'Stores') do %>
+      <%= link_to_with_icon 'shopping-cart', Spree.t(:stores), stores_admin_product_path(@product) %>
+    <% end %>",
   :disabled => false)
