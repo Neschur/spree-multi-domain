9616b90dd2e0c479fb708c65ae1bd9c03019a11d
Siarhei Hanchuk <siarheihanchuk@gmail.com>
2015-04-09 18:45:31 +0300

    Assign current_store on create

--- app/controllers/spree/admin/products_controller_decorator.rb
+++ app/controllers/spree/admin/products_controller_decorator.rb
@@ -1,4 +1,11 @@
 Spree::Admin::ProductsController.class_eval do
+  create.after :assign_current_store
+
   def stores
   end
+
+  protected
+    def assign_current_store
+      @product.stores = [current_store]
+    end
 end
