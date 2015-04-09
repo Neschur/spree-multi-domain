086b841bfa47b5362002b303aede1644767a57ed
Siarhei Hanchuk <siarheihanchuk@gmail.com>
2015-04-09 18:32:29 +0300

    Fixed bug with store assign

--- app/controllers/spree/admin/products_controller_decorator.rb
+++ app/controllers/spree/admin/products_controller_decorator.rb
@@ -1,12 +1,4 @@
 Spree::Admin::ProductsController.class_eval do
-  update.before :set_stores
-
   def stores
   end
-
-  private
-  def set_stores
-    @product.store_ids = nil unless params[:product].key? :store_ids
-  end
-
 end
